9d16d80add6db86ae5f3ca2e8fd86a4ce1fd459a
Anand-S23 <anandsroopsingh23@gmail.com>
2023-11-08 16:55:40 -0500

    Refactor REPL

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
-use std::io::{self, Write};
+use std::{io::{self, Write}, process::exit};
+
+enum CommandRunStatus {
+    Success,
+    Unrecognized
+}
+
+const COMMAND_EXIT: &str = ".exit";
 
 fn main() {
-    let exit: String = String::from(".exit");
-
-
     loop {
         print!("mirror > "); 
         io::stdout().flush().unwrap();
@@ -15,11 +19,25 @@ fn main() {
             .expect("Failed to read line");
 
         input_buffer = input_buffer.trim().to_string();
-
-        if input_buffer.eq(&exit) {
-            break;
-        } else {
-            println!("Unrecognized command '{input_buffer}'\n")
+        if input_buffer.len() != 0 && input_buffer.chars().nth(0).expect("Could not parse command") == '.' {
+            match run_command(&input_buffer) {
+                CommandRunStatus::Success => continue,
+                CommandRunStatus::Unrecognized => {
+                    println!("Unrecognized command '{input_buffer}'\n");
+                    continue;
+                },
+            }
         }
+        
+        println!("Test Coverage"); 
     }
 }
+
+fn run_command(command: &String) -> CommandRunStatus {
+    if command.eq(&COMMAND_EXIT.to_string()) {
+        exit(0);
+    } 
+
+    CommandRunStatus::Unrecognized
+}
+
